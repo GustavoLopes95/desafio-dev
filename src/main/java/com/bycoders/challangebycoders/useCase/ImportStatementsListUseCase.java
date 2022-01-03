37a67d22732b64b60bc659e2daa6bd0290e9a562
Gustavo Lopes <lgalopes20@gmail.com>
2022-01-03 08:38:20 -0300

    Add strategy to extract CNAB

--- src/main/java/com/bycoders/challangebycoders/useCase/ImportStatementsListUseCase.java
+++ src/main/java/com/bycoders/challangebycoders/useCase/ImportStatementsListUseCase.java
@@ -7,6 +7,8 @@ import com.bycoders.challangebycoders.domain.factories.OperationFactory;
 import com.bycoders.protobuf.StatementsProtos;
 import org.springframework.stereotype.Service;
 
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -23,18 +25,32 @@ public class ImportStatementsListUseCase {
     }
 
     private List<Statement> parse(List<String> statements) {
-        return statements.stream().map(statement -> {
-            var operationType = OperationFactory.make(OperationTypeEnum.valueOf(Integer.valueOf(statement.substring(0, 1))));
-            var transactionDate = LocalDateTime.parse(statement.substring(1, 9).concat(" ").concat(statement.substring(42, 48)), DateTimeFormatter.ofPattern("yyyyMMdd HHmmss"));
-            var value = Double.valueOf(statement.substring(9, 18));
-            var cardNumber = statement.substring(30, 41);
+        return statements.stream().map(statement -> this.factory(statement)).collect(Collectors.toList());
+    }
 
-            var document = statement.substring(19, 29);
-            var client = statement.substring(48, 61);
-            var clientName = statement.substring(62, 80);
+    private Statement factory(String statement) {
+        var operationType = OperationFactory.make(OperationTypeEnum.valueOf(Integer.valueOf(statement.substring(0, 1))));
+        var transactionDate = LocalDateTime.parse(statement.substring(1, 9).concat(" ").concat(statement.substring(42, 48)), DateTimeFormatter.ofPattern("yyyyMMdd HHmmss"));
+        var value = Double.valueOf(statement.substring(9, 18));
+        var cardNumber = statement.substring(30, 41);
 
-            var entity = new Statement(1L, operationType, value, cardNumber, transactionDate);
-            return entity;
-        }).collect(Collectors.toList());
+        var document = statement.substring(19, 29);
+
+
+//        var client = statement.substring(48, 61);
+        var client = this.extractStatement(statement, 48, 61);
+//        var clientName = statement.substring(62, 80);
+        var clientName = this.extractStatement(statement, 62, 80);
+        return  new Statement(1L, operationType, value, cardNumber, transactionDate);
+    }
+
+    private String extractStatement(String statement, Integer startIndex, Integer maxIndex) {
+        CharacterIterator it = new StringCharacterIterator(statement);
+        it.setIndex(startIndex-1);
+        var sb = new StringBuilder();
+        while(it.getIndex() < maxIndex || Character.isWhitespace(it.current())){
+            sb.append(it.next());
+        }
+        return sb.toString();
     }
 }
